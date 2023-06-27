5e07990c406e410c02bba34b9b54cb18e62823a0
Shashankdk23 <117855805+Shashankdk23@users.noreply.github.com>
2023-06-27 15:53:01 +0530

    Add files via upload

---
+++ bfss.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
+
+void bfs(int v)
+{
+    for (i = 1; i <= n; i++)
+        if (a[v][i] && !visited[i])
+            q[++r] = i;
+    if (f <= r)
+    {
+        visited[q[f]] = 1;
+        bfs(q[f++]);
+    }
+}
+
+void main()
+{
+    int v;
+    printf("\n Enter the number of vertices:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        q[i] = 0;
+        visited[i] = 0;
+    }
+    printf("\n Enter adjacent matrix:\n");
+    for (i = 1; i <= n; i++){
+        for (j = 1; j <= n; j++){
+            printf("enter at %d,%d",i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+
+    printf("\n Enter the starting vertex:");
+    scanf("%d", &v);
+    bfs(v);
+    printf("\n The node connected are:\n");
+    for (i = 1; i <= n; i++){
+              if (visited[i]){
+            printf("%d ,", i);
+              }
+    }
+
+}
